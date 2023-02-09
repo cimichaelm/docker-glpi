2dae1ed8dfdb618e5478e4c84e80c60738430c0b
Michael Moscovitch <michaelm@corp.citenet.net>
2023-02-09 11:34:46 -0500

    update plugins

--- scripts/site.sh
+++ scripts/site.sh
@@ -23,7 +23,9 @@ siteconfig()
     pluginlist="https://github.com/pluginsGLPI/mreporting/releases/download/1.8.2/glpi-mreporting-1.8.2.tar.bz2 https://github.com/glpi-project/glpi-inventory-plugin/releases/download/1.0.6/glpi-glpiinventory-1.0.6.tar.bz2 https://github.com/yllen/reports/releases/download/v1.16.0/glpi-reports-1.16.0.tar.gz https://github.com/pluginsGLPI/datainjection/releases/download/2.12.0/glpi-datainjection-2.12.0.tar.bz2 https://github.com/yllen/pdf/releases/download/v3.0.0/glpi-pdf-3.0.0.tar.gz https://github.com/InfotelGLPI/mydashboard/releases/download/2.1.4/glpi-mydashboard-2.1.4.tar.bz2 https://github.com/pluginsGLPI/barcode/releases/download/2.7.1/glpi-barcode-2.7.1.tar.bz2 https://github.com/pluginsGLPI/genericobject/releases/download/2.14.1/glpi-genericobject-2.14.1.tar.bz2 https://github.com/cconard96/glpi-camerainput-plugin/releases/download/v2.0.2/glpi-camerainput-v2.0.2.tar.bz2 https://github.com/yllen/behaviors/releases/download/v2.7.2/glpi-behaviors-2.7.2.tar.gz"
     downloadlist=""
     wgetoptions="--quiet"
-    
+    pluginsbzinst="glpi-barcode-2.7.1.tar.bz2 glpi-behaviors-2.7.2.tar.gz glpi-camerainput-v2.0.2.tar.bz2 glpi-datainjection-2.12.0.tar.bz2 glpi-genericobject-2.14.1.tar.bz2 glpi-mreporting-1.8.2.tar.bz2 glpi-mydashboard-2.1.4.tar.bz2"
+    pluginsgzinst="glpi-pdf-3.0.0.tar.gz glpi-reports-1.16.0.tar.gz"
+    extractprog=bzcat    
 }
 
 siteconfig
